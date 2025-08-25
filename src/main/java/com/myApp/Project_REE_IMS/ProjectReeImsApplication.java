34ed9910fa0f5ef5a6fa21f0b968b51bdc8d435d
Anshika Saxena <developer.anshika467@gmail.com>
2025-08-25 11:56:39 +0530

    Initializing Project

---
+++ src/main/java/com/myApp/Project_REE_IMS/ProjectReeImsApplication.java
@@ -0,0 +1,13 @@
+package com.myApp.Project_REE_IMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReeImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectReeImsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myApp/Project_REE_IMS/ProjectReeImsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myApp.Project_REE_IMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReeImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
